2296e7fe9f71d1bf66e8a70ec5c838693c21781c
Jeffrey A <jalhassan@umassd.edu>
2016-11-04 10:35:49 -0400

    update

--- HomeWork/2/Inventory.java
+++ HomeWork/2/Inventory.java
@@ -3,7 +3,7 @@ public class Inventory {
 
 
 	//variables for the Inventory class
-	private Item itemArray; //stores a instances of a Item
+	private Item itemArray[] = new Item[100]; //stores a instances of a Item
 	private int totalItem = 0;
 
 	//Methods for the Inventroy class
@@ -12,13 +12,34 @@ public class Inventory {
 		return totalItem;
 	}
 
+
 	// retunrs an instances of a Item
 	public Item getItem(int index){
-		return itemArray;
+
+		//error check
+		if(index < 0 || index >= totalItem)
+		{
+			return null;
+		}
+		else
+		{
+			return itemArray[index];
+		}
 	}
 
 	// assgine item to a variable
 	public void addItem(Item newItem){
+
+		// check to see if item has been added
+		if(newItem == null)
+		{
+			System.out.println("Item not added");
+		}
+		else
+		{
+			itemArray[totalItem] = newItem;
+			totalItem++; //update totalNumber
+		}
 
 	}
 
