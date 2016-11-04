ed97ed28414f6e1879c4f7933800171fce824a8e
Jeffrey A <jalhassan@umassd.edu>
2016-11-04 10:40:18 -0400

    update on ITI

--- HomeWork/2/InventoryTrackerInterface.java
+++ HomeWork/2/InventoryTrackerInterface.java
@@ -3,7 +3,10 @@ public class InventoryTrackerInterface {
 
 	public static void main(String[] args){
 
+		 //Inventory inv;
+
 		Item a = new Item("box",1,100.0,"level");
+		Item c = new Item("box",1,100.0,"level");
 		System.out.println(a.getName());
 		System.out.println(a.getQuantity());
 		System.out.println(a.getPrice());
@@ -11,7 +14,14 @@ public class InventoryTrackerInterface {
 
 
 		Inventory b = new Inventory();
-		b.getTotalNumberOfItems();
+
+
+		b.addItem(a);
+		b.addItem(a);
+		b.addItem(c);
+
+
+		System.out.println("Totoal number of Item " + b.getTotalNumberOfItems());
 	}
 
 }
