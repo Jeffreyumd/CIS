b4af8d619fae1ea2bdf7d6fd40eb7bb26c824a22
Jeffrey A <jalhassan@umassd.edu>
2016-11-06 15:31:51 -0500

    homeWork2 update

--- HomeWork/2/Inventory.java
+++ HomeWork/2/Inventory.java
@@ -1,9 +1,9 @@
-
+import java.io.*;
 public class Inventory {
 
 
 	//variables for the Inventory class
-	private Item itemArray; //stores a instances of a Item
+	private Item itemArray[] = new Item[100]; //stores a instances of a Item
 	private int totalItem = 0;
 
 	//Methods for the Inventroy class
@@ -12,29 +12,94 @@ public class Inventory {
 		return totalItem;
 	}
 
+
 	// retunrs an instances of a Item
+	// Get the item's stores data from the Array
 	public Item getItem(int index){
-		return itemArray;
+
+		//error check
+		if(index < 0 || index >= totalItem)
+		{
+			return null;
+		}
+		else
+		{
+			return itemArray[index];
+		}
 	}
 
 	// assgine item to a variable
+	// add Item into the Array
 	public void addItem(Item newItem){
+
+		// check to see if item is a Instace of a Item, then check if the Item has been added
+		if(newItem == null)
+		{
+			System.out.println("Item not added");
+		}
+		else
+		{
+			itemArray[totalItem] = newItem;
+			totalItem++; //update totalNumber
+		}
 
 	}
 
 
 	//create and save inventory data to a file
 	public void saveInventoryToFile(String fileName){
+		/// write name, quality, price, upc on diffrent lines on the textfile
+		try{
+			// create instances of the txt file
+		 FileWriter fwriter = new FileWriter(fileName+".txt", true);
+		 PrintWriter item = new PrintWriter(fwriter);
 
+		 // write the info of the item on the text file
+		 for(int i=0; i < totalItem; i++){
+		 item.println("");
+		 item.println("Name: "+getItem(i).getName());
+		 item.println("Price: "+getItem(i).getPrice());
+		 item.println("Quantity: "+getItem(i).getQuantity());
+		 item.println("UPC: "+getItem(i).getUPC());
+
+		 }
+
+		 fwriter.close();
+		 item.close();
+
+
+		} catch(Exception e){
+
+			System.out.println("Something went wrong\nFile could not Write");
+		}
 	}
 
 	// load inventory data from a file
 	public void loadInventoryFromFile(String fileName){
 
+
+		//Read created file, if file does not exites create one
+
+		try{
+
+			FileReader file = new FileReader(fileName); //read the file
+			BufferedReader breader = new BufferedReader(file); //print the collected string in a line
+
+			String line = "";
+
+			while((line = breader.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+
+			file.close();
+			breader.close();
+
+
+		} catch(Exception e){
+			System.out.println("Something went wrong\nfile could not Read");
+		}
+
 	}
-
-
-
-
 
 }
--- HomeWork/2/InventoryTrackerInterface.java
+++ HomeWork/2/InventoryTrackerInterface.java
@@ -1,17 +1,107 @@
-
+import java.util.Scanner;
 public class InventoryTrackerInterface {
 
 	public static void main(String[] args){
 
-		Item a = new Item("box",1,100.0,"level");
-		System.out.println(a.getName());
-		System.out.println(a.getQuantity());
-		System.out.println(a.getPrice());
-		System.out.println(a.getUPC());
+		Scanner input = new Scanner(System.in);
 
+		boolean active = true; // check the user status
+		int num; // user option for switch statment
 
 		Inventory b = new Inventory();
-		b.getTotalNumberOfItems();
+
+		while(active == true)
+		{
+			// variables to store user input
+			String name;
+			String upc;
+			int quantity;
+			double price;
+
+			// Ask User what they wanna do
+			System.out.println("1. Add item to Inventory");
+			System.out.println("2. Get an items info");
+			System.out.println("3. Save Inventory to file");
+			System.out.println("4. Loard Inventory from file");
+			System.out.println("5. Exit");
+
+			num = input.nextInt();
+			switch(num)
+			{
+
+				case 1:
+
+					System.out.println("Enter new Item name");
+					input.nextLine();
+					name = input.nextLine();
+
+
+					System.out.println("Enter new item quantity");
+					quantity = input.nextInt();
+
+					System.out.println("Enter new item price");
+					price = input.nextDouble();
+
+					System.out.println("Enter new item UPC");
+					input.nextLine();
+					upc = input.nextLine();
+
+					// instance of the object
+					Item a = new Item(name,quantity,price,upc);
+
+					// add item to array
+					b.addItem(a);
+					break;
+
+				case 2:
+
+					int i = b.getTotalNumberOfItems() - 1;
+
+					if(i < 0)
+					{
+						System.out.println("No item in the Inventory");
+					} else{
+
+						//ask user for item number
+						System.out.println("Which item info would you like [0-" + i +"]" ); // display what item number is available
+
+						//store item number in num
+						num = input.nextInt();
+
+						// get item data from Inventory class
+						b.getItem(num);
+						System.out.println("Name: "+b.getItem(num).getName());
+						System.out.println("Price: "+b.getItem(num).getPrice());
+						System.out.println("Quantity: "+b.getItem(num).getQuantity());
+						System.out.println("UPC: "+b.getItem(num).getUPC());
+						}
+					break;
+
+				case 3:
+					// call inventory to save item to file
+					b.saveInventoryToFile("Inventory");
+					System.out.println("Inventory Saved");
+					break;
+
+				case 4:
+					// diaplay all the data in the inventory
+					b.loadInventoryFromFile("Inventory.txt");
+					break;
+
+				case 5:
+					// exit out of the loop
+					System.out.print("Exit: Goodbye !");
+					active = false;
+					break;
+
+					default:
+						System.out.println("Input not supported try again");
+						break;
+		  }
+		}
+		
+		input.close();
+
 	}
 
 }
