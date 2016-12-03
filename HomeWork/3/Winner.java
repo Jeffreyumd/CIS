1c7760675cb8ed5d75a498a685d7ea7605bd6dde
Jeffrey A <jalhassan@umassd.edu>
2016-12-03 16:23:52 -0500

    update on homeWork 3

--- HomeWork/3/Winner.java
+++ HomeWork/3/Winner.java
@@ -1,1 +1,33 @@
 
+public class Winner{
+	
+	private String team;
+	private int year;
+	
+	private Winner()
+	{
+		/**
+		 * Make default constuctor private
+		 */
+	}
+	
+	/**
+	 * public constuctor
+	 * @param team
+	 * @param year
+	 */
+	public Winner(String team, int year)
+	{
+		this.team = team;
+		this.year = year;
+	}
+	
+	public String getTeam(){
+		return team;
+	}
+	
+	public int getYear(){
+		return year;
+	}
+	
+}/// end of winner class
