d356a7236bb6cd7441e35e71bf42324d68b71849
Jeffrey A <jalhassan@umassd.edu>
2016-12-08 15:19:05 -0500

    Create LoShu

---
+++ HomeWork/3/LoShuMagic.java
@@ -0,0 +1,16 @@
+
+public class LoShuMagic {
+	
+	public static boolean test(int[][] Array)
+	{
+		for(int i=0; i < Array.length; i++)
+		{
+			for(int j=0; j < Array.length; j++)
+			{
+				System.out.println(Array[i][j]);
+			}
+		}
+		 return true;
+	}
+
+}
